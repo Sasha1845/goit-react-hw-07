38cc114df5f2249317a191bd517af6e314894f53
Sasha_Ischuk <ipz231_ios1@student.ztu.edu.ua>
2025-04-12 10:12:02 +0300

    ddz 7 update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,76 +1,97 @@
 "use client";
-
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addContact } from "../../redux/contactsOps.js";
 import { selectContacts } from "../../redux/contactsSlice.js";
 import styles from "./ContactForm.module.css";
+import { Formik, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const ContactForm = () => {
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
   const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .matches(
+        /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+        "Name may contain only letters, apostrophe, dash and spaces."
+      )
+      .required("Name is required"),
+    number: Yup.string()
+      .matches(
+        /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+        "Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+      )
+      .required("Number is required"),
+  });
 
+  const initialValues = {
+    name: "",
+    number: "",
+  };
+
+  const handleSubmit = (values, { resetForm }) => {
     const isDuplicate = contacts.some(
-      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+      (contact) => contact.name.toLowerCase() === values.name.toLowerCase()
     );
 
     if (isDuplicate) {
-      alert(`${name} is already in contacts.`);
+      alert(`${values.name} is already in contacts.`);
       return;
     }
 
-    dispatch(addContact({ name, number }));
-    reset();
-  };
-
-  const reset = () => {
-    setName("");
-    setNumber("");
+    dispatch(addContact({ name: values.name, number: values.number }));
+    resetForm();
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <div className={styles.formGroup}>
-        <label className={styles.label} htmlFor="name">
-          Name
-        </label>
-        <input
-          className={styles.input}
-          type="text"
-          name="name"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces."
-          required
-        />
-      </div>
-      <div className={styles.formGroup}>
-        <label className={styles.label} htmlFor="number">
-          Number
-        </label>
-        <input
-          className={styles.input}
-          type="tel"
-          name="number"
-          id="number"
-          value={number}
-          onChange={(e) => setNumber(e.target.value)}
-          pattern="\+?\d{1,4}?[-.\s]?$$?\d{1,3}?$$?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-      </div>
-      <button className={styles.button} type="submit">
-        Add contact
-      </button>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ handleSubmit, isSubmitting }) => (
+        <div className={styles.form}>
+          <div className={styles.formGroup}>
+            <label className={styles.label} htmlFor="name">
+              Name
+            </label>
+            <Field className={styles.input} type="text" name="name" id="name" />
+            <ErrorMessage
+              name="name"
+              component="div"
+              className={styles.error}
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label className={styles.label} htmlFor="number">
+              Number
+            </label>
+            <Field
+              className={styles.input}
+              type="tel"
+              name="number"
+              id="number"
+            />
+            <ErrorMessage
+              name="number"
+              component="div"
+              className={styles.error}
+            />
+          </div>
+
+          <button
+            className={styles.button}
+            type="button"
+            onClick={() => handleSubmit()}
+            disabled={isSubmitting}
+          >
+            Add contact
+          </button>
+        </div>
+      )}
+    </Formik>
   );
 };
 
